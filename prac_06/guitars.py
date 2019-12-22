7957f2bca22276d48787262ec71bba2745b58c79
nyan-soe <58061109+nyan-soe@users.noreply.github.com>
2019-12-22 17:52:39 +0800

    Update guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,14 +3,22 @@ from prac_06.guitar import Guitar
 
 def main():
     guitars = []
-    name = "Start"
-    while name:
-        name = input("Name:")
+    name = input("Name:")
+    while name != "":
         year = int(input("Year:"))
         cost = float(input("Cost:"))
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(guitar, " added.")
+
+        name = input("Name:")
+
+    print("These are my guitars:")
+
+    for i, guitar in enumerate(guitars):
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""  # set vintage_string by using ternary operator
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                  vintage_string))
 
 
 if __name__ == "__main__":
